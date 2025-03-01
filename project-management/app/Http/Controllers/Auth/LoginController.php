f00b31cf96319865bbdd618b464521cb550d8661
athira-shibu <athira.shibu@dibasys.com>
2025-03-01 11:26:07 +0530

    added user create request

---
+++ project-management/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+/*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    */
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+
+            $user = auth()->user();
+
+            return response()->json(compact('token'));
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Could not create token'], 500);
+        }
+    }
+}
---
+++ project-management/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class RegisterController extends Controller
+{
+    public function register(): JsonResponse
+    {
+        
+    }
+}
