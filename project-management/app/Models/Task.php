5d0bfe483839c577d1a5e1c40972291f3b48100e
athira-shibu <athira.shibu@dibasys.com>
2025-03-01 11:08:35 +0530

    modified task model

--- project-management/app/Models/Task.php
+++ project-management/app/Models/Task.php
@@ -1,11 +1,41 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ * 
+ * @property string $name
+ * @property string $status
+ */
 class Task extends Model
 {
     use HasFactory;
+
+    protected $table = 'tasks';
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class, 'project_id', 'id');
+    }
+
+    public function getProject(): Project
+    {
+        return $this->getRelationValue('project');
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->getAttribute('id'),
+            'name' => $this->getAttribute('name'),
+            'status' => $this->getAttribute('status'),
+            'project' => $this->getProject()->toArray(),
+        ];
+    }
 }
